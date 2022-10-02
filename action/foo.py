c0cd9702e741146ced0e007def8c3b85f9d3faba
0x4448 <86135470+0x4448@users.noreply.github.com>
2022-10-02 20:31:04 +0000

    Update warning message

--- action/foo.py
+++ action/foo.py
@@ -5,7 +5,7 @@ import os
 def main():
     print("Hello!")
     print(os.environ.get("GITHUB_ACTION", "$GITHUB_ACTION not found"))
-    print("::warning::This is a warning")
+    print("::warning file=FILENAME,title=Warning Title::This is a warning")
     print("::error::This is an error")
 
 
