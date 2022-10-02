5c263249d4e7114773e746d53a96ccfb8b39e4f7
0x4448 <86135470+0x4448@users.noreply.github.com>
2022-10-02 20:41:45 +0000

    Add notice

--- action/foo.py
+++ action/foo.py
@@ -5,6 +5,8 @@ import os
 def main():
     print("Hello world!")
     print(os.environ.get("GITHUB_ACTION", "$GITHUB_ACTION not found"))
+    print("::notice file=app.js,line=1,col=5,endColumn=7::Missing semicolon")
+    print("::warning file=app.js,line=1,endLine=5,title=TITLE::MESSAGE")
     print("::warning file=FILENAME,title=Warning Title::This is a warning")
     print("::error::This is an error")
 
